d31739a998c9260243579f1f5a3ec357e2d869d4
kamensky.ra <kamensky.ra@gmail.com>
2023-11-05 00:35:31 +0200

    Initial Commit

---
+++ SwiftUI-Weather/SwiftUI_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_WeatherApp.swift
+//  SwiftUI-Weather
+//
+//  Created by Роман Каменский on 05.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
